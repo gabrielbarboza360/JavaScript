cfb7700ccc7660fb04e0aa86342efc6280b9d0b9
Gabriel Barboza <93885322+gabrielbarboza360@users.noreply.github.com>
2022-01-30 10:21:57 -0300

    new

---
+++ contador/teste7.js
@@ -0,0 +1,35 @@
+var beep = new Audio();
+beep.src = "beep.mp3";
+beep.play();
+
+var explosao = new Audio()
+explosao.src = "explosao.mp3";
+explosao.play();
+
+var check = false;
+
+function inicia(){
+    if (check == false){
+    var time1 = setInterval(function(){start()}, 2000);
+    var time2 = setTimeout(function(){preto()}, 23200);
+    var contador = 10;
+
+    function start(){
+        beep.play();  
+        window.document.getElementById('txt').innerHTML = contador;
+         if(contador == 0){
+             clearInterval(time1);
+             explosao.play();
+             window.document.getElementById('imagem1').src = "explosion.gif";
+             document.getElementById('txt').innerHTML = "GAME OVER"
+}
+        contador --;
+    
+}
+}
+    check = true;
+}
+
+function preto(){
+    document.getElementById('imagem1').src = "preto.png"
+}
